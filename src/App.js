7bf93e5b3b22442d27ad6fbead9a83ff7fccc254
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-12-29 22:35:21 -0600

    Create the SwipeButtons component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import TinderCards from './components/TinderCards';
+import SwipeButtons from './components/SwipeButtons';
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
       <TinderCards />
+      <SwipeButtons />
     </div>
   );
 };
